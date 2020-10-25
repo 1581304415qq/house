f9d8b695c0234eec66b6ff5213ec259f9c2a12e9
1581304415qq <1581304415@qq.com>
2020-10-25 20:22:12 +0800

    bk

--- src/App3.vue
+++ src/App3.vue
@@ -1,33 +1,19 @@
 <template>
   <div id="app">
-    <home-page v-if='isHome'/>
-    <user-page v-else/>
-    <div class="tab_view">
-    <div class='view_item' @click="goto('/home')">主页</div>
-    <div class='view_item' @click="goto('/user')">用户</div>
-    </div>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import homePage from "./pages/home.vue";
-import userPage from "./pages/user.vue";
 export default {
   name: 'app',
   data () {
     return {
-      isHome:true,
     }
   },
   methods:{
-    goto:function(e){
-      if (e==='/home') this.isHome=true
-      else if (e==='/user') this.isHome=false
-    }
   },
   components:{
-    homePage,
-    userPage,
   }
 }
 </script>
@@ -40,18 +26,6 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-.tab_view{
-  position: absolute;
-  bottom: 0;
-  width: 100%;
-  height: 1.2rem;
-  display: flex;
-  justify-content: center;
-  align-items: center; 
-}
-.view_item{
-  flex-grow: 1;
 }
 h1, h2 {
   font-weight: normal;
--- src/pages/tabView.vue
+++ src/pages/tabView.vue
@@ -1,26 +1,65 @@
 <template>
-    <div class='tabview'>tabview </div>
+  <div id="tab_view">
+    <home-page v-if='isHome'/>
+    <user-page v-else/>
+    <div class="tab_bar">
+    <div class='view_item' @click="goto('/home')">主页</div>
+    <div class='view_item' @click="goto('/user')">用户</div>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-    	data(){
-            return{
-            }
-        },
-        created(){
-           
-        },
-        mounted(){
-            
-        },
-        computed: {
-        },
-        methods: {
-        },
+import homePage from "./home.vue";
+import userPage from "./user.vue";
+export default {
+  name: 'tab_view',
+  data () {
+    return {
+      isHome:true,
     }
+  },
+  methods:{
+    goto:function(e){
+      if (e==='/home') this.isHome=true
+      else if (e==='/user') this.isHome=false
+    }
+  },
+  components:{
+    homePage,
+    userPage,
+  }
+}
 </script>
 
 <style>
-   
+.tab_bar{
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 1.2rem;
+  display: flex;
+  justify-content: center;
+  align-items: center; 
+}
+.view_item{
+  flex-grow: 1;
+}
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
 </style>
