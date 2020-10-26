51c996516a606245127191f194ee6db34bdd5459
1581304415qq <1581304415@qq.com>
2020-10-26 18:58:58 +0800

    bk

--- src/pages/consult.vue
+++ src/pages/consult.vue
@@ -1,60 +1,85 @@
 <template>
     <div class='consult'>
         <navigation-bar/>
-        <div>
-            <div>所在小区</div>
+
+        <el-form :model="form" :rules="yearRules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+          <el-form-item label="活动名称" prop="name">
+            <el-input v-model="form.detailAddress"></el-input>
+          </el-form-item>
+          <el-form-item>
+              
+          </el-form-item>
+
+        </el-form>
+        <div class="consult_form">
+        <div class="consult_item">
+            <div class="label">所在小区</div>
             <el-cascader
               v-model="form.road"
               :options="options"></el-cascader>
         </div>
-        <div>详细地址
+        <div class="consult_item">
+        <div class="label">详细地址</div>
             <el-input v-model="form.detailAddress" placeholder="请输入内容"></el-input>
-        </div>
-        <div>土地级别</div>
+        
+      </div>
+        <div class="consult_item">
+        <div class="label">土地级别</div>
             <el-select v-model="form.level" placeholder="请选择">
             <el-option
-              v-for="item in address"
+              v-for="item in level"
               :key="item.value"
               :label="item.label"
               :value="item.value">
             </el-option>
           </el-select>
-        <div>房产类型 
+        </div>
+        <div class="consult_item">
+        <div class="label">房产类型 </div>
             <el-select v-model="form.type" placeholder="请选择">
             <el-option
-              v-for="item in address"
+              v-for="item in type"
               :key="item.value"
               :label="item.label"
               :value="item.value">
             </el-option>
           </el-select>
         </div>
-        <div>是否为电梯房
+        <div class="consult_item">
+        <div class="label">是否为电梯房</div>
             <div>
-              <el-radio v-model="form.elevator" label="1">备选项</el-radio>
-              <el-radio v-model="form.elevator" label="2">备选项</el-radio>
+              <el-radio v-model="form.elevator" label="1">是</el-radio>
+              <el-radio v-model="form.elevator" label="2">否</el-radio>
             </div>
         </div>
-        <div>建成年份
+        <div class="consult_item">
+        <div class="label">建成年份</div>
             <el-input v-model='form.year'></el-input>
         </div>
-        <div>产权证面积
+        <div class="consult_item">
+        <div class="label">产权证面积</div>
             <el-input v-model='form.propertyarea'></el-input>
         </div>
-        <div>公摊面积
+        <div class="consult_item">
+        <div class="label">公摊面积</div>
             <el-input v-model='form.publicarea'></el-input>
         </div>
-        <div>土地面积
+        <div class="consult_item">
+        <div class="label">土地面积</div>
             <el-input v-model='form.area'></el-input>
         </div>
-        <div>是否为学区房
+        <div class="consult_item">
+        <div class="label">是否为学区房</div>
             <div>
-              <el-radio v-model="form.schooldistrict" label="1">备选项</el-radio>
-              <el-radio v-model="form.schooldistrict" label="2">备选项</el-radio>
+              <el-radio v-model="form.schooldistrict" label="1">是</el-radio>
+              <el-radio v-model="form.schooldistrict" label="2">否</el-radio>
             </div>
         </div>
 
+        <div class="consult_button">
         <el-button id="consult_button" type="primary" @click='consult'>估价</el-button>
+      </div>
+    </div>
     </div>
 </template>
 
@@ -65,30 +90,64 @@
     	data(){
             return{
               options: [{
-                value: 'zhinan',
-                label: '指南',
+                value: '001',
+                label: '上杭',
                 children: [{
-                  value: 'shejiyuanze',
-                  label: '设计原则',
+                  value: '001',
+                  label: '陵城',
+                },
+                {
+                  value: '002',
+                  label: '城西',
+                },{
+                  value: '003',
+                  label: '城东',
+                },{
+                  value: '004',
+                  label: '城南',
+                }]},
+                {
+                value: '002',
+                label: '武平',
+                children: [{
+                  value: '001',
+                  label: '陵城',
+                },
+                {
+                  value: '002',
+                  label: '城西',
+                },{
+                  value: '003',
+                  label: '城东',
+                },{
+                  value: '004',
+                  label: '城南',
                 }]}],
-                address: [{
+                level:[{
                   value: '1',
-                  label: '黄金糕'
+                  label: '商业用地'
                 }, {
                   value: '2',
-                  label: '双皮奶'
+                  label: '住房用地'
                 }, {
                   value: '3',
-                  label: '蚵仔煎'
+                  label: '工业用地'
                 }, {
                   value: '4',
-                  label: '龙须面'
+                  label: '农业用地'
+                }],
+                type: [{
+                  value: '1',
+                  label: '店铺'
                 }, {
-                  value: '5',
-                  label: '北京烤鸭'
+                  value: '2',
+                  label: '自建房'
                 }, {
-                  value: '6',
-                  label: '北京烤鸭'
+                  value: '3',
+                  label: '商品房子'
+                }, {
+                  value: '4',
+                  label: '老宅'
                 }],
                 value:'',
 
@@ -96,6 +155,12 @@
                 radio:'',
                 form:{road:[],detailAddress:'',level:'',type:'',elevator:false,
                       year:'2020',area:0,publicarea:0,propertyarea:0,schooldistrict:false
+                },
+                yearRules:{
+                     name: [
+                        { required: false, message: '请输入4位数字', trigger: 'blur' },
+                        { min: 4, max: 4, message: '长度在 4个字符', trigger: 'blur' }
+                      ],
                 }
             }
         },
@@ -136,7 +201,27 @@
         height: 100%;
         width: 100%;
    }
-
+   .consult_form{
+    display:flex;
+    flex-direction: column;
+    min-height:70%;
+   }
+   .consult_item{
+    display:flex;
+    margin: .3rem;
+    align-items: center;
+    justify-items: flex-start;
+    justify-content: flex-start;
+   }
+   .consult_item .label{
+    width: 3rem;
+    text-align: left;
+   }
+   .consult_button{
+    bottom: 1rem;
+    width: 100%;
+    height: 1rem;
+    }
    #consult_button{
         background-color:#cf4233;
    }
